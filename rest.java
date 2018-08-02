4dec1aba118a79e7afac65c98e7f4243818f7531
bandianand408 <bandianand408@gmail.com>
2018-08-02 16:36:47 +0000

    first file

---
+++ rest.java
@@ -0,0 +1,5 @@
+branches:-
+dev1(file2.java)
+    i.  merge file to master(i.git checkout master , git merge branchname)
+                    and push to github
+    ii. directly push from branch to github
